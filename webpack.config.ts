9ef13b1beee6be82b718a227f14032c3f4368b62
Tim Gafarov <timu.ryan@yandex.ru>
2025-09-04 00:37:05 +0300

    feat: add alias @

--- webpack.config.ts
+++ webpack.config.ts
@@ -20,7 +20,8 @@ const config = (env: BuildEnv = {}): Configuration => {
   const paths: BuildPaths = {
     output: path.resolve(__dirname, 'build'),
     entry: path.resolve(__dirname, 'src', 'index.tsx'),
-    html: path.resolve(__dirname, 'public', 'index.html')
+    html: path.resolve(__dirname, 'public', 'index.html'),
+    src: path.resolve(__dirname, 'src'),
   }
 
   const config: Configuration = buildWebpack({
